416391b4d8ed08ff1635d997fc5b7b3332c6e0fe
gustavo <necocheagustavo@gmail.com>
2025-12-10 23:33:37 -0800

    Initial Commit

---
+++ StoneLedger/StoneLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoneLedgerApp.swift
+//  StoneLedger
+//
+//  Created by csuftitan on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StoneLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
